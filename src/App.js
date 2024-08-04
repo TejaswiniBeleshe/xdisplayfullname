b12c1451016bd3922654789a4c268c9c38e7a520
TejaswiniBeleshe <sonu2201.beleshe@gmail.com>
2024-08-04 17:29:57 +0530

    added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import './App.css';
 function App() {
   let [fname,setFname] = useState('');
   let [lname,setLname] = useState('');
-  let [fullname,setFullname] = useState('')
+  let [fullname,setFullname] = useState('');
+  let [check,setCheck] = useState(false);
   const handleData = (event)=>{
     event.preventDefault();
     // if(fname !=="" || lname !== ""){
     //   let full = fname+" "+lname;
     //   setFullname(`Full Name: ${full}`);
     // }else{
-      setFullname('')
+      // setFullname('')
     // }
-   
+   setCheck((prev)=>!prev);
   }
   return (
     <div className="App">
@@ -24,7 +25,7 @@ function App() {
         <p><label htmlFor="last">Last Name:</label><input type="text" id="last" name="lastname" value={lname} onChange={(e)=>setLname(e.target.value)} required/></p>
         <button type="submit">Submit</button>
        </form>
-       <p>{fullname}</p>
+       <p>{check?`Full Name: ${fname} ${lname}`:""}</p>
 
     </div>
   );
