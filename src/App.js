cbefc9d78f51d5a5d4c75ed1c4095793263c5ef3
TejaswiniBeleshe <sonu2201.beleshe@gmail.com>
2024-08-04 14:53:33 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -8,17 +8,21 @@ function App() {
   let [fullname,setFullname] = useState('')
   const handleData = (event)=>{
     event.preventDefault();
-    let full = fname+" "+lname;
-    setFullname(`Full Name: ${full}`);
-    // setFname('');
-    // setLname(''); 
+    if(fname !=="" || lname !== ""){
+      let full = fname+" "+lname;
+      setFullname(`Full Name: ${full}`);
+      // setFname('');
+      // setLname(''); 
+      
+    }
+   
   }
   return (
     <div className="App">
        <h1>Full Name Display</h1>
        <form onSubmit={(e)=>handleData(e)}>
-        <><label htmlFor="first">First Name:</label><input type="text" id="first" value={fname} onChange={(e)=>setFname(e.target.value)} required/></>
-        <><label htmlFor="last">Last Name:</label><input type="text" id="last" value={lname} onChange={(e)=>setLname(e.target.value)} required/></>
+        <><label htmlFor="first">First Name:</label><input type="text" id="first" value={fname} onChange={(e)=>setFname(e.target.value)}/></>
+        <><label htmlFor="last">Last Name:</label><input type="text" id="last" value={lname} onChange={(e)=>setLname(e.target.value)}/></>
         <button type="submit">Submit</button>
        </form>
        <p>{fullname}</p>
